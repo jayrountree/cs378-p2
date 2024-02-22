b542e7c80bd12d1f5fb6bebde76010fb529e4af0
Jay Rountree <jaynr22@gmail.com>
2024-02-21 22:44:09 -0600

    Updates

---
+++ precache-manifest.bc3db78af1028426c85f1776ba84eb25.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04f6fefdc8a91b3273bf8ea4751ec22",
+    "url": "/cs378-p2/index.html"
+  },
+  {
+    "revision": "5deeddd1d5f2b325d951",
+    "url": "/cs378-p2/static/css/2.99718f4b.chunk.css"
+  },
+  {
+    "revision": "955b4a12c43fea0f824a",
+    "url": "/cs378-p2/static/css/main.f00c3b06.chunk.css"
+  },
+  {
+    "revision": "5deeddd1d5f2b325d951",
+    "url": "/cs378-p2/static/js/2.7dae1e62.chunk.js"
+  },
+  {
+    "revision": "1876019e6f8cf7ca83e2",
+    "url": "/cs378-p2/static/js/3.0e4cdb98.chunk.js"
+  },
+  {
+    "revision": "955b4a12c43fea0f824a",
+    "url": "/cs378-p2/static/js/main.e73542ec.chunk.js"
+  },
+  {
+    "revision": "e95edbf31b6db1e5af9e",
+    "url": "/cs378-p2/static/js/runtime~main.a5da5581.js"
+  },
+  {
+    "revision": "70c9ee4d79d365f76f05999ef253cd52",
+    "url": "/cs378-p2/static/media/gyoza.70c9ee4d.png"
+  },
+  {
+    "revision": "091b71c1bcf33ad4f3f24466364655c1",
+    "url": "/cs378-p2/static/media/katsu-curry.091b71c1.png"
+  },
+  {
+    "revision": "482038157777258de3bc10f78dae9575",
+    "url": "/cs378-p2/static/media/logo.48203815.png"
+  },
+  {
+    "revision": "14aab84c0d5a7b5c4f4527b547b93c6a",
+    "url": "/cs378-p2/static/media/matcha-cake.14aab84c.png"
+  },
+  {
+    "revision": "3203d3f58c718befc0e356f3d30ebea9",
+    "url": "/cs378-p2/static/media/mochi.3203d3f5.png"
+  },
+  {
+    "revision": "dfe1570992f147ccfd0e44fed72ebf7c",
+    "url": "/cs378-p2/static/media/okonomiyaki.dfe15709.png"
+  },
+  {
+    "revision": "b3bfd8d3d0f3a5ba9d4ad3445849abfb",
+    "url": "/cs378-p2/static/media/ramen.b3bfd8d3.png"
+  },
+  {
+    "revision": "4e5cbf890cb984a38b777b880d3eff13",
+    "url": "/cs378-p2/static/media/sashimi.4e5cbf89.png"
+  },
+  {
+    "revision": "0190450a885ca9ebfb0ab627d4e86ff3",
+    "url": "/cs378-p2/static/media/sushi.0190450a.png"
+  },
+  {
+    "revision": "23fe78b9ec6a950be24054c64150fe6e",
+    "url": "/cs378-p2/static/media/takoyaki.23fe78b9.png"
+  },
+  {
+    "revision": "989c62e807d7014b9e5372e12309a9ce",
+    "url": "/cs378-p2/static/media/yakitori.989c62e8.png"
+  }
+]);
